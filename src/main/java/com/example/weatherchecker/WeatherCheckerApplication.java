8a6f374f84e257b34103b1189143ef0bdeb88269
aysedemirel <aysedemirel1996@gmail.com>
2021-11-29 21:04:47 +0300

    [REFC] Refactor workspace

---
+++ src/main/java/com/example/weatherchecker/WeatherCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherCheckerApplication.class, args);
+	}
+
+}
